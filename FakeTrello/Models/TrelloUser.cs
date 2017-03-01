0ad1b2d7a64a15a59daad0f497f456f77c8a568d
mgteutsch <mgt0117@gmail.com>
2017-02-28 19:15:05 -0800

    In class examples, Board and TrelloUser

---
+++ FakeTrello/Models/TrelloUser.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+//Entity is our Object Relational Mapper (OBM)
+
+namespace FakeTrello.Models
+{
+    public class TrelloUser
+    {
+        [Key] //Had to Build the project first, then the red squiggle appeared and I chose the "Using...." solution
+        public int TrelloUserId { get; set; } //This is the Primary Key
+
+        //Stacking of properties applies to multiple annotations
+        //to the following property
+        [MinLength(10)]
+        [MaxLength(60)]
+        public string Email { get; set; }
+
+        [MinLength(10)]
+        [MaxLength(60)]
+        public string Username { get; set; }
+
+        [MinLength(10)]
+        [MaxLength(60)]
+        public string FullName { get; set; }
+
+        //This is a simple reference to ANOTHER table (ApplicationUser, which is an object, too)
+        public ApplicationUser BaseUser { get; set; } //This creates the 1 to 1 relationship between ApplicationUser & TrelloUser
+
+        //This is a "navigation property"
+        public List<Board> Boards { get; set; } // 1 to Many (boards) relationship
+    }
+}
